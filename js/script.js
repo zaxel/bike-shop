dda31292c4b4bb59cc722a72becd4152e4eeb068
zaxel <sae11.z.alex@gmail.com>
2020-09-19 22:17:24 +0100

    bike shop ready

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.testimonials_carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        arrows : false
+        // adaptiveHeight: true
+    });
+  });
+  
